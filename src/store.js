1c2b03cbdc87ebada5e895c22103342d75c8266a
smhailey <smhailey@msn.com>
2019-07-16 13:57:29 -0600

    init

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
+
+let api = axios.create({
+  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=Yv4Bal2HaprOxVA9ZTW1V0OBembeEXhykz6UWfwl'
+})
 
 export default new Vuex.Store({
   state: {
